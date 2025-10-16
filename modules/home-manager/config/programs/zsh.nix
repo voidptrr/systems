f881f2bb46636138869a60b93c77e7d1ca469fac
voidptrr <bruno.tommaso@protonmail.com>
2025-10-16 21:25:29 +0100

    feat: detect if in nix-shell

--- modules/home-manager/config/programs/zsh.nix
+++ modules/home-manager/config/programs/zsh.nix
@@ -9,11 +9,15 @@
 
     initContent = lib.mkOrder 500 ''
       autoload -Uz vcs_info
-      zstyle ':vcs_info:git:*' formats '%F{red}[%b]%f '
+      zstyle ':vcs_info:git:*' formats '%F{red}[%b]%f'
 
       precmd() {
         vcs_info
-        PROMPT="%F{red}%n%f in %F{yellow}%~%f %F{red}''${vcs_info_msg_0_}%f%# "
+        if [ -n "''${IN_NIX_SHELL-}" ]; then
+          PROMPT="%F{red}%n%f in %F{yellow}%~%f %F{red}''${vcs_info_msg_0_}%f%F{blue}[nix]%f %# "
+        else
+          PROMPT="%F{red}%n%f in %F{yellow}%~%f %F{red}''${vcs_info_msg_0_}%f %# "
+        fi
       }
     '';
   };
