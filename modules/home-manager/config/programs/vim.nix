4b42966652103702b94ee10a959eb916b7ddcc91
voidptrr <bruno.tommaso@protonmail.com>
2025-10-16 05:00:36 +0100

    feat: vim statusline

--- modules/home-manager/config/programs/vim.nix
+++ modules/home-manager/config/programs/vim.nix
@@ -48,6 +48,27 @@
       nnoremap <leader>gr :Git rebase<cr>
       nnoremap <leader>gP :Git pull<cr>
       nnoremap <leader>gp :Git push<cr>
+
+
+      " Status line
+      set laststatus=2
+
+      hi User1 ctermfg=green ctermbg=black
+      hi User2 ctermfg=yellow ctermbg=black
+      hi User3 ctermfg=red ctermbg=black
+      hi User4 ctermfg=blue ctermbg=black
+      hi User5 ctermfg=white ctermbg=black
+
+      set statusline=
+      set statusline +=%1*\ %n\ %*            "buffer number
+      set statusline +=%5*%{&ff}%*            "file format
+      set statusline +=%3*%y%*                "file type
+      set statusline +=%4*\ %<%F%*            "full path
+      set statusline +=%2*%m%*                "modified flag
+      set statusline +=%2*%=%5l%*             "current line
+      set statusline +=%2*/%L%*               "total lines
+      set statusline +=%2*%4v\ %*             "virtual column number
+      set statusline +=%2*0x%04B\ %*          "character under cursor
     '';
   };
 }
