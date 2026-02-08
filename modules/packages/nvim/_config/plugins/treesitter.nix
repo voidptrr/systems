e5cdf1cb8eb2b02c3e93a8dfbb01e938ea8b186e
Tommaso Bruno <bruno.tommaso@protonmail.com>
2026-02-08 03:37:57 +0000

    chore: treesitter packages

--- modules/packages/nvim/_config/plugins/treesitter.nix
+++ modules/packages/nvim/_config/plugins/treesitter.nix
@@ -3,7 +3,11 @@
     enable = true;
     highlight.enable = true;
     grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      c
+      javascript
       nix
+      rust
+      typescript
       yaml
     ];
   };
