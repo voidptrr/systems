600295cdf77af4987be35a0466cc08ebca18f0ae
voidptrr <bruno.tommaso@protonmail.com>
2026-02-12 23:37:18 +0000

    chore: move firefox policies to own file

---
+++ modules/internet/firefox/_policies.nix
@@ -0,0 +1,27 @@
+{
+  AppAutoUpdate = false;
+  BackgroundAppUpdate = false;
+  DisableTelemetry = true;
+  DisableFirefoxStudies = true;
+  DisableFirefoxAccounts = true;
+  DisablePocket = true;
+  DontCheckDefaultBrowser = true;
+  OfferToSaveLogins = false;
+  EnableTrackingProtection = {
+    Value = true;
+    Locked = true;
+    Cryptomining = true;
+    Fingerprinting = true;
+    EmailTracking = true;
+  };
+  SearchSuggestEnabled = false;
+  UserMessaging = {
+    ExtensionRecommendations = false;
+    FeatureRecommendations = false;
+    Locked = true;
+    MoreFromMozilla = false;
+    SkipOnboarding = true;
+    UrlbarInterventions = false;
+    WhatsNew = false;
+  };
+}
--- modules/internet/firefox/base.nix
+++ modules/internet/firefox/base.nix
@@ -11,33 +11,7 @@
         then null
         else pkgs.firefox
       );
-      policies = {
-        AppAutoUpdate = false;
-        BackgroundAppUpdate = false;
-        DisableTelemetry = true;
-        DisableFirefoxStudies = true;
-        DisableFirefoxAccounts = true;
-        DisablePocket = true;
-        DontCheckDefaultBrowser = true;
-        OfferToSaveLogins = false;
-        EnableTrackingProtection = {
-          Value = true;
-          Locked = true;
-          Cryptomining = true;
-          Fingerprinting = true;
-          EmailTracking = true;
-        };
-        SearchSuggestEnabled = false;
-        UserMessaging = {
-          ExtensionRecommendations = false;
-          FeatureRecommendations = false;
-          Locked = true;
-          MoreFromMozilla = false;
-          SkipOnboarding = true;
-          UrlbarInterventions = false;
-          WhatsNew = false;
-        };
-      };
+      policies = import ./_policies.nix;
       profiles.default = {
         name = "voidptr";
         bookmarks = {
